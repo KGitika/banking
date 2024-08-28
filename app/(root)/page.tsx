37f3ae2766b7c5a4e1050526f891bfde0a817572
Gitika Kalal <gitikakalal21@gmail.com>
2024-08-27 22:07:45 -0700

    Implement the comp lete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Gitika', lastName: 'Kalal', email: 'gitikakalal@gmail.com'};
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext = "Access and manage your account and transaction effciently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        
+        RT
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}    //{account?.transactions}
+        banks={[{currentBalance:129.0}, {}] }      //{accountsData?.slice(0, 2)}
+      />
+    </section>
   )
 }
 
