ef138b61eaa25902531027678af4b7d6bb51403d
Gitika Kalal <gitikakalal21@gmail.com>
2024-09-02 13:38:42 -0700

    finalize the app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,23 @@
 import HeaderBox from '@/components/HeaderBox'
-// import RecentTransactions from '@/components/RecentTransactions';
+import RecentTransactions from '@/components/RecentTransactions';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
-// import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
-  // const accounts = await getAccounts({ 
-  //   userId: loggedIn.$id 
-  // })
+  const accounts = await getAccounts({ 
+    userId: loggedIn.$id 
+  })
 
-  // if(!accounts) return;
+  if(!accounts) return;
   
-  // const accountsData = accounts?.data;
-  // const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
-  // const account = await getAccount({ appwriteItemId })
+  const account = await getAccount({ appwriteItemId })
 
   return (
     <section className="home">
@@ -30,19 +30,19 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
             subtext="Access and manage your account and transactions efficiently."
           />
 
-          {/* <TotalBalanceBox 
+          <TotalBalanceBox 
             accounts={accountsData}
             totalBanks={accounts?.totalBanks}
             totalCurrentBalance={accounts?.totalCurrentBalance}
-          /> */}
+          />
         </header>
 
-        {/* <RecentTransactions 
+        <RecentTransactions 
           accounts={accountsData}
           transactions={account?.transactions}
           appwriteItemId={appwriteItemId}
           page={currentPage}
-        /> */}
+        />
       </div>
 
       <RightSidebar 
